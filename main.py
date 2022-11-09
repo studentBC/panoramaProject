dd8fb71fcd1934a127a4b911f42e17e6c41f8309
studentBC <r04546032@ntu.edu.tw>
2022-11-08 18:09:43 -0800

    finish foreground extract but it is not perfect which depends on content of video

--- main.py
+++ main.py
@@ -8,23 +8,71 @@ import shutil
 import numpy as np
 global width, height, frameCount, fps, foreGround, backGround
 
+#using BFS to get foreground and background
+def getForeground(i, j, img, fgmask, fg):
+    if fgmask[i][j] == 127:
+        return
+    fg.append([i, j])
+
+
 #we extract foreground and background
 #https://docs.opencv.org/3.4/d4/dee/tutorial_optical_flow.html
 #https://pythonprogramming.net/mog-background-reduction-python-opencv-tutorial/
 def extractImages():
-    for i in range(frameCount):
+    foreGround=[]
+    backGround=[]
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
+    rect = (300,120,470,350)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    background_subtr_method = cv2.bgsegm.createBackgroundSubtractorGSOC()
+    for a in range(frameCount):
         success,img = cap.read()
         if not success:
             print("error in extracting foreground !!!")
         #cv2.imwrite("./tmp/frame%d.jpg" % count, img)     # save frame as JPEG file    
         fgmask = fgbg.apply(img)  
-        cv2.imshow('frame',fgmask)
-        if i > 10000:
-            cv2.imwrite("./tmp/frame%d.jpg" % i, img)
-            #foreGround.append(img)
-        elif i > 10050:
-            break
+        fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel) 
+        #cv2.grabCut(img,fgmask, rect,  bgdModel, fgdModel,  5, cv2.GC_INIT_WITH_RECT)
+        #bg = fgbg.getBackgroundImage(img)
+        #cv2.imshow('fg mask',fgmask)
+        #cv2.imshow('frame', bg)
+        #cv2.imwrite("./tmp/frame%d.jpg" % i, fgmask)
+        #fg, bg = [[[0]*3]*width]*height, [[[0]*3]*width]*height
+        fg, bg = np.zeros((height, width, 3)), np.zeros((height, width, 3))
+        prev = 0
+        for i in range(0, height):
+            for j in range(0, width):
+                #print(fgmask[i][j], end = " ")
+                if int(fgmask[i][j]) == 0 and prev == 0:
+                    bg[i][j][0] = img[i][j][0]
+                    bg[i][j][1] = img[i][j][1]
+                    bg[i][j][2] = img[i][j][2]
+                else:
+                    fg[i][j][0] = img[i][j][0]
+                    fg[i][j][1] = img[i][j][1]
+                    fg[i][j][2] = img[i][j][2]
+                prev = fgmask[i][j]
+                #print(fg[i][j], img[i][j])
+                #print()
+        #sometimes the color that cv2 img show is not correct and not realtime
+        #when this happen just save processed image to verify
+        #cv2.imshow('frame', img)
+        #cv2.imshow('fg ',np.asarray(fg))
+        #cv2.imshow('bg ', fg)
+        #cv2.imshow('frame', bg)
+        #cv2.imwrite("./tmp/frame%d.jpg" % a, bg)
+        foreGround.append(fg)
+        backGround.append(bg)
         #image.append(img)
+
+        # another model which uses BFS
+        # pass the frame to the background subtractor
+        # foreground_mask = background_subtr_method.apply(img)
+        # # obtain the background without foreground mask
+        # background_img = background_subtr_method.getBackgroundImage()
+        # cv2.imshow('lol', foreground_mask)
+        #cv2.imwrite("./tmp/frame%d.jpg" % i, background_img)
         k = cv2.waitKey(30) & 0xff
         if k == 27:
             break
@@ -37,8 +85,8 @@ if __name__=="__main__":
     shutil.rmtree("./tmp")
     os.mkdir("./tmp")
     if cap.isOpened():
-        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         fps =  cap.get(cv2.CAP_PROP_FPS)
         print("we have "+str(width) + " "+ str(height)+" "+ str(frameCount) + " " + str(fps))
@@ -53,5 +101,5 @@ if __name__=="__main__":
     # Create a video a new video by defining a path in the panorama image, the foreground objects move in time synchronized manner.
 
     # save video
-    cv2.destroyAllWindoes()
     cap.release()
+    cv2.destroyAllWindows()

cade5a228f01ca0cc88a89fcfa3a9b86500d8b46
studentBC <r04546032@ntu.edu.tw>
2022-11-15 23:02:00 -0800

    add two model

--- foreground_extraction.py
+++ foreground_extraction.py
@@ -137,3 +137,78 @@ class ForegroundExtractor:
             fgmask = np.where((fgmask == 2) | (fgmask == 0), 0, 1).astype('uint8')
             fgmasks.append(fgmask)
         return np.array(fgmasks)
+        
+    def get_foreground_mask_dof(self, frames):
+        prvs = cv2.cvtColor(frames[0], cv2.COLOR_BGR2GRAY)
+        hsv = np.zeros_like(frames[0])
+        hsv[..., 1] = 255
+        fgmasks = []
+        #print("enter get_foreground_mask_dof")
+        for i in tqdm(range(1, len(frames))):
+            next = cv2.cvtColor(frames[i], cv2.COLOR_BGR2GRAY)
+            flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+            hsv[..., 0] = ang*180/np.pi/2
+            hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+            bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+            #h, s, v1 = cv2.split(bgr)
+            #cv2.imshow("gray-image",bgr[:,:, 2:3])
+            #now we convert every fram to 0 or 255
+            (thresh, im_bw) = cv2.threshold(bgr[:,:, 2:3], 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+            # cv2.imshow('frame2', im_bw)
+            # k = cv2.waitKey(30) & 0xff
+            # replay any > 0 to 1
+            im_bw[im_bw > 0] = 1
+            prvs = next
+            fgmasks.append(im_bw)
+        # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+        # fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
+        
+        # for frame in tqdm(frames):
+        #     fgmask = fgbg.apply(frame)
+        #     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+        #     fgmask = np.where((fgmask == 255), 1, 0).astype('uint8')
+        #     fgmasks.append(fgmask)
+        fgmasks.append(fgmasks[-1])
+        return np.array(fgmasks)
+        
+    def get_foreground_mask_lko(self, frames):
+        # params for ShiTomasi corner detection
+        feature_params = dict( maxCorners = 100,
+                            qualityLevel = 0.3,
+                            minDistance = 7,
+                            blockSize = 7 )
+        # Parameters for lucas kanade optical flow
+        lk_params = dict( winSize  = (15, 15),
+                        maxLevel = 2,
+                        criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        # Create some random colors
+        color = np.random.randint(0, 255, (100, 3))
+        # Take first frame and find corners in it
+        old_frame = frames[0]
+        old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+        p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+        # Create a mask image for drawing purposes
+        mask = np.zeros_like(old_frame)
+
+        for i in tqdm(range(1, len(frames))):
+
+            frame_gray = cv2.cvtColor(frames[i], cv2.COLOR_BGR2GRAY)
+            # calculate optical flow
+            p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+            # Select good points
+            if p1 is not None:
+                good_new = p1[st==1]
+                good_old = p0[st==1]
+            # draw the tracks
+            for i, (new, old) in enumerate(zip(good_new, good_old)):
+                a, b = new.ravel()
+                c, d = old.ravel()
+                mask = cv2.line(mask, (int(a), int(b)), (int(c), int(d)), color[i].tolist(), 2)
+                frames[i] = cv2.circle(frames[i], (int(a), int(b)), 5, color[i].tolist(), -1)
+            img = cv2.add(frames[i], mask)
+            cv2.imshow('frame', img)
+            k = cv2.waitKey(30) & 0xff
+            # Now update the previous frame and previous points
+            old_gray = frame_gray.copy()
+            p0 = good_new.reshape(-1, 1, 2)

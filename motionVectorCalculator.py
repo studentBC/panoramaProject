d1908a8bcf4822dc8223975b1a8682ba4a2cfde5
Chia-Hao Liu <chliu.official@gmail.com>
2022-11-16 02:20:52 -0800

    optimize motion vector

--- motionVectorCalculator.py
+++ motionVectorCalculator.py
@@ -4,31 +4,23 @@ import math
 import numpy as np
 
 class motionVector:
-    def getMAD(self, x, y, cur, prev, k):
+    def getMAD(self, prevframe, curframe, y, x, bh, bw, k):
         #our search distance is 2K+1 for k is given by input
         #our right point will be i+16, j+16
-        w, h = cur.shape[1], cur.shape[0]
-        ex, ey = min(x+k, w), min(y+k, h)
+        height, width, _ = curframe.shape
+        curblock = curframe[y: y+bh, x: x+bw, 0]
         #searching block area
         #left upper point and right down point of searching block area
-        xstart, ystart, xend, yend = max(0, x-k), max(y-k, 0), min(ex+k, w), min(ey+k, h) 
-        #print(x, y, xstart, ystart, xend, yend)
-        total, small, avg = 16*16, sys.maxsize, 0
-        targetVector = []
-        for a in range(ystart, yend):
-            for b in range(xstart, xend):
-                if a == x and y == b:
-                    continue
-                c, d, sum = a, b, 0
-                for i in range(y, ey):
-                    for j in range(x, ex):
-                        sum+=abs(cur[i][j]-prev[c][d])/total #prevent overflow problem
-                        d+=1
-                    c+=1
-                #avg = sum/total
-                avg = sum
-                #print(sum, avg)
-                if small > avg:
-                    small = avg
-                    targetVector = [a, b]
-        return small, targetVector
+        xstart, ystart, xend, yend = max(x-k, 0), max(y-k, 0), min(x+k, width-bw), min(y+k, height-bh)
+
+        mad_values = []
+        for i in range(ystart, yend + 1):
+            tmp = []
+            for j in range(xstart, xend + 1):
+                tmp.append(np.mean(np.absolute(curblock - prevframe[i: i+bh, j: j+bw, 0])))
+            mad_values.append(tmp)
+
+        min_index = np.argmin(mad_values)
+        target_y, target_x = ystart + min_index // (xend - xstart + 1), xstart + (min_index % (xend - xstart + 1))
+
+        return [y - target_y, x - target_x]

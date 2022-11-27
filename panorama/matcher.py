be877ef779df18bc6fc39150573c7e7f53786299
Lorne Chen <laurenchen0631@gmail.com>
2022-11-26 18:56:31 -0800

    find transformation

--- panorama/matcher.py
+++ panorama/matcher.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 class matcher:
@@ -8,7 +9,7 @@ class matcher:
         #self.surf = cv2.SURF_create()
         self.sift = cv2.SIFT_create()
         FLANN_INDEX_KDTREE = 0
-        index_params = dict(algorithm=0, trees=5)
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
         search_params = dict(checks=50)
         self.flann = cv2.FlannBasedMatcher(index_params, search_params)
 
@@ -39,3 +40,27 @@ class matcher:
         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         kp, des = self.sift.detectAndCompute(gray, None)
         return {'kp': kp, 'des': des}
+
+    def searchTransformation(self, src: np.ndarray,
+                             dst: np.ndarray) -> np.ndarray | None:
+        srcFeatures = self.getSIFTFeatures(src)
+        dstFeatures = self.getSIFTFeatures(dst)
+        matches = self.flann.knnMatch(srcFeatures['des'],
+                                      dstFeatures['des'],
+                                      k=2)
+        good = []
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good.append(m)
+
+        if len(good) > 4:
+            src_pts = np.float32([
+                srcFeatures['kp'][m.queryIdx].pt for m in good
+            ]).reshape(-1, 1, 2)
+            dst_pts = np.float32([
+                dstFeatures['kp'][m.trainIdx].pt for m in good
+            ]).reshape(-1, 1, 2)
+            transformation, _ = cv2.findHomography(dst_pts, src_pts,
+                                                   cv2.RANSAC, 5.0)
+            return transformation
+        return None
